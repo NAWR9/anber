baea53ae646438249464d3da6be12d31ded69bc5
Osamah <osamah.saddeq@gmail.com>
2024-08-06 19:26:29 +0300

    Created HomePage UI. The ability to toggle between dark and light mode

--- lib/Pages/home_page.dart
+++ lib/Pages/home_page.dart
@@ -1,4 +1,7 @@
+import 'package:anber/Components/my_button.dart';
+import 'package:anber/Themes/theme_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -7,6 +10,82 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Theme.of(context).colorScheme.surface,
+      body: SafeArea(
+        child: Column(
+          children: [
+            // Dark Mode switch
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: IconButton(
+                      onPressed: () =>
+                          Provider.of<ThemeProvider>(context, listen: false)
+                              .toggleTheme(),
+                      icon: Icon(
+                        Provider.of<ThemeProvider>(context).isDarkMode
+                            ? Icons.light_mode_outlined
+                            : Icons.dark_mode_rounded,
+                        color: Theme.of(context).colorScheme.inversePrimary,
+                        size: 30,
+                      )),
+                )
+              ],
+            ),
+            const SizedBox(
+              height: 50,
+            ),
+            // Logo
+            Center(
+              child: Icon(Icons.calculate_outlined,
+                  size: 90,
+                  color: Theme.of(context).colorScheme.inversePrimary),
+            ),
+            // Title
+            const SizedBox(
+              height: 30,
+            ),
+            Center(
+              child: Text(
+                "عنبر",
+                style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 40,
+                    color: Theme.of(context).colorScheme.inversePrimary),
+              ),
+            ),
+            const SizedBox(
+              height: 50,
+            ),
+            // column of buttons [new game, history, exit]
+            Center(
+              child: Column(
+                children: [
+                  MyButton(
+                    text: 'Play',
+                    onTap: () {},
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  MyButton(
+                    text: 'History',
+                    onTap: () {},
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  MyButton(
+                    text: 'Exit',
+                    onTap: () {},
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
